2bcbe33cc6cefdf12d9e89a1b09698c5bba9ea22
Lolmaia <131406007+Lolmaia@users.noreply.github.com>
2023-09-25 19:14:13 -0300

    Add foto
    Chamada de api

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -1,3 +1,33 @@
 console.log("Teste do JS");
 
+function getUsuarios(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then( (resp1) => resp1.json())
+    .then( (resp2) => mostraUsuarios(resp2))
+}
 
+function mostraUsuarios(dados){
+    console.log(dados);
+    var container = document.getElementById('listaUsuarios');
+    dados.forEach( (usuario) => {
+        let paragrafo = document.createElement('p');
+        paragrafo.innerText = usuario.name;
+        container.appendChild(paragrafo)
+    })
+}
+
+function getFoto(){
+    fetch("https://picsum.photos/200/300")
+    .then( (resp) => mostrarFoto(resp))
+}
+
+function mostrarFoto(dado){
+    console.log(dado);
+    let container = document.getElementById('fotoDia');
+    let imagem = document.createElement('img'); 
+    imagem.src = dado.url;
+    container.appendChild(imagem);
+}
+
+getUsuarios();
+getFoto();
